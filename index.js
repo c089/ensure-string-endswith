d9b6671850871abfa5d65598eca479a0ae7dd878
Alexander Schmidt <alexanderschmidt1@gmail.com>
2015-02-16 14:27:38 +0100

    Improve formatting

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 'use strict';
+
 module.exports = function (characterToEndWith) {
     if (characterToEndWith.length !== 1) {
         throw new Error('Expected a character, got: ' + characterToEndWith);
     }
 
     return function (string) {
+        var lastCharacter;
+
         if (typeof string !== 'string') {
             throw new Error('Expected a string, got: ' + string);
         }
-        var lastCharacter = string[string.length - 1];
-        if (lastCharacter === characterToEndWith) return string;
+
+        lastCharacter = string[string.length - 1];
+
+        if (lastCharacter === characterToEndWith) {
+            return string;
+        }
+
         return string + characterToEndWith;
     }
 };
